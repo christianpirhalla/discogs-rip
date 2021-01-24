90c9eba2b9b2aa112f44a13301a9b61f3b0adbc2
Christian Pirhalla <chrispir@udel.edu>
2021-01-23 22:36:15 -0500

    first commit

---
+++ src/main/java/com/example/discogsrip/DiscogsRipApplication.java
@@ -0,0 +1,13 @@
+package com.example.discogsrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscogsRipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscogsRipApplication.class, args);
+	}
+
+}
