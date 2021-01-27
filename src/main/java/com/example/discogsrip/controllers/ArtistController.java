2fb8440aea24592db6e44f689cd2c32cab53fba2
Christian Pirhalla <chrispir@udel.edu>
2021-01-26 20:21:47 -0500

    started controller tests

--- src/main/java/com/example/discogsrip/controllers/ArtistController.java
+++ src/main/java/com/example/discogsrip/controllers/ArtistController.java
@@ -4,6 +4,7 @@ import com.example.discogsrip.entities.Artist;
 import com.example.discogsrip.services.ArtistService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,16 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-@RequestMapping("https://localhost8080/artist")
+//@RequestMapping("https://localhost8080/artist")
 public class ArtistController {
 
     @Autowired
     ArtistService artistService;
 
-    @PostMapping
+    @PostMapping("/artist")
     public ResponseEntity<Artist> createArtist(@RequestBody Artist newArtist){
 
-        return null;
+        return new ResponseEntity<>(artistService.createArtist(newArtist), HttpStatus.CREATED);
     }
 
 }
--- src/test/java/com/example/discogsrip/controllers/ArtistControllerTest.java
+++ src/test/java/com/example/discogsrip/controllers/ArtistControllerTest.java
@@ -1,7 +1,51 @@
 package com.example.discogsrip.controllers;
 
+import com.example.discogsrip.DiscogsRipApplication;
+import com.example.discogsrip.entities.Artist;
+import com.example.discogsrip.services.ArtistService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(controllers = ArtistController.class)
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = DiscogsRipApplication.class)
 class ArtistControllerTest {
+
+    @MockBean
+    private ArtistService artistService;
+
+    //@MockBean Person
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testSaveArtist() throws Exception{
+
+        Artist mockArtist = Artist.builder().id(123L).artistName("The Wacks").country("Estonia").build();
+
+        Mockito.when(artistService.createArtist(mockArtist)).thenReturn(mockArtist);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/artist", mockArtist)
+        .contentType(MediaType.APPLICATION_JSON)
+        .characterEncoding("UTF-8")
+        .content("insert JSON string here"))
+                .andExpect(MockMvcResultMatchers.status().is(201))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("id", Matchers.is(123)));
+    }
 
 }
